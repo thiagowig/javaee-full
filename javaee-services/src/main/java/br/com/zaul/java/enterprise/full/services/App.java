c18e567dc93443ac05848227ce79e24610cf0a51
Thiago Fonseca <dev.thiago@gmail.com>
2014-06-03 18:56:32 -0300

    First Commit

---
+++ javaee-services/src/main/java/br/com/zaul/java/enterprise/full/services/App.java
@@ -0,0 +1,14 @@
+package br.com.zaul.java.enterprise.full.services;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ javaee-services/src/main/java/br/com/zaul/java/enterprise/full/services/activator/RestActivator.java
@@ -0,0 +1,9 @@
+package br.com.zaul.java.enterprise.full.services.activator;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestActivator extends Application {
+
+}
